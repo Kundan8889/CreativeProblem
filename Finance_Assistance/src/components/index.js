7985deb8d7b86d5146becf60e06afa2ed01db923
Kundan Rathore <Kundan8889@users.noreply.github.com>
2024-11-14 01:41:16 +0530

    updated

---
+++ Finance_Assistance/src/components/index.js
@@ -0,0 +1,24 @@
+
+import Navbar from "./Navbar";
+import Billing from "./Billing.jsx";
+import CardDeal from "./CardDeal";
+import Business from "./Business.jsx";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero.jsx";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
